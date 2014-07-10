953afef7fd61913b24af8d1dcd4124133d2ef49d
Brent S.A. Cowgill <brent@blismedia.com>
2014-07-10 09:27:20 +0100

    jshint-json-to-comment-oneline.pl added // comment check to regex

--- bin/jshint-json-to-comment-oneline.pl
+++ bin/jshint-json-to-comment-oneline.pl
@@ -1,2 +1,2 @@
 # one liner to convert a .jshintrc JSON file into a /*jshint */ comment block
-perl -pne 's/\{/\/*jshint /xms && chomp ; s{\s*\"}{}xmsg && chomp; s/\}/\n\*\//xms; s{\ +}{ }xmsg; s{,}{, }xmsg' .jshintrc-sloppy 
+perl -pne 's{\s*\/\/.*\z}{}xms; s/\{/\/*jshint /xms && chomp ; s{\s*\"}{}xmsg && chomp; s/\}/\n\*\//xms; s{\ +}{ }xmsg; s{,}{, }xmsg' .jshintrc-sloppy 
